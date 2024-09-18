1c3e6df3aeef4f22ae00b4dfbaca2b77280860ae
Viet Hoang <trinhviethoang16@gmail.com>
2024-09-19 02:53:53 +0700

    feat: feat: solve LeetCode - Best Time To Buy And Sell Stock

---
+++ aloalo.js
@@ -0,0 +1,30 @@
+//Bai tu lam lai tim so nguyen to, cho mang so nguyen duong in ra mang so nguyen to.
+function checkPrimeNumber(testNumber) {
+    if (testNumber === 2) {
+      return true;
+    } else if (testNumber <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(testNumber); i++) {
+      if (testNumber % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function findPrimeNumber(inputArray) {
+    if (!Array.isArray(inputArray)) {
+      return "The input must be Array";
+    }
+    let result = [];
+    for (let i = 0; i < inputArray.length; i++) {
+      if (checkPrimeNumber(inputArray[i])) {
+        result.push(inputArray[i]);
+      }
+    }
+    return result;
+  }
+  
+  console.log(checkPrimeNumber(66));
+  console.log(findPrimeNumber([3, 2, 1, 10, 9, 77, 6, 97]));
+  
